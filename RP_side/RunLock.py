7f4340bda2e80f5aa467bae4c76e1f5da5d0e0ea
deveshq <devesh123456.40@gmail.com>
2026-03-13 14:20:57 +0530

    Added NBs

--- RP_side/RunLock.py
+++ RP_side/RunLock.py
@@ -5,4 +5,7 @@ from RP_Lock import *
 host, port = '192.168.0.104', 5000
 Lock = RP_Server(host, port, 5065, RP_mode = 'monitor')
 Lock.setup_server(loop=False)
-Lock.start_server()
+Lock.start_server()host = os.environ.get("RP_LOCK_HOST", "192.168.0.101")
+port = int(os.environ.get("RP_LOCK_PORT", "5000"))
+loop_port = int(os.environ.get("RP_LOCK_LOOP_PORT", "5065"))
+mode = os.environ.get("RP_LOCK_MODE", "scan")
